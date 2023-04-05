9cf8dda11b0ea6661e37228dc827e9cfe54ff104
Gubi Samuel (240617) <240617@vutbr.cz>
2023-04-05 14:35:59 +0200

    Vivado code created, readme added, sources added

---
+++ MorseCode/MorseCode.srcs/sources_1/new/but_to_morse.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/05/2023 02:27:45 PM
+-- Design Name: 
+-- Module Name: but_to_morse - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity but_to_morse is
+    Port ( CLK100MHz : in STD_LOGIC;
+    
+    );
+end but_to_morse;
+
+architecture Behavioral of but_to_morse is
+
+begin
+
+
+end Behavioral;
